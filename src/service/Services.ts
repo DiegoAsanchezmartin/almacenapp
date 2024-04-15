7e5ffb7208b4522ec8e585a2df8cb0a697ea9e8d
DiegoAsanchezmartin <17391039@utcancun.edu.mx>
2024-04-15 11:29:23 -0500

    correciones del login
    se cambio la estructura de como se manejaban los datos en el login y se corrigio el almacenamiento del token

--- src/service/Services.ts
+++ src/service/Services.ts
@@ -3,25 +3,26 @@ import type IUser from '@/interfaces/IUser'; // Asegúrate de importar la interf
 const api_url = 'http://187.157.236.135:5000';
 
 export default {
-  async login(name: string, password: string): Promise<IUser | null> {
+  async login(username: string, password: string): Promise<IUser | null> {
     try {
       const response = await fetch(`${api_url}/auth`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ name, password })
+        body: JSON.stringify({ username, password })
       });
 
       if (!response.ok) {
         throw new Error('Credenciales incorrectas');
       }
       
-      const { token } = await response.json();
+      const  token  = await response.json();
+      localStorage.setItem('accessToken', token);
       console.log('Token de acceso:', token);
       return token;
     } catch (error) {
-      console.error('Error al autenticar:', error.message);
+      console.error('Error al autenticar:', (error as Error).message);
       return null;
     }
   }
