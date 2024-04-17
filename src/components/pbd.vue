0f74d7b4b8090b8aafe15a5c075ce9d93bdedc75
CarlosAlejandro411 <carlos.alex.martinez411@gmail.com>
2024-04-16 19:58:28 -0500

    ya

--- src/components/pbd.vue
+++ src/components/pbd.vue
@@ -5,20 +5,19 @@
     <div class="filters">
       <input type="text" v-model="filters.nombre" placeholder="Filtrar por nombre" list="nombre-list">
       <datalist id="nombre-list">
-        <option v-for="product in products" :value="product.producto" :key="product._id"/>
+        <option v-for="product in products" :value="product.nombre" :key="product._id"/>
       </datalist>
+      <select v-model="filters.categoria">
+        <option disabled value="">Seleccione una categoría</option>
+        <option v-for="categoria in uniqueCategorias" :key="categoria">{{ categoria }}</option>
+      </select>
       <select v-model="filters.estado">
         <option disabled value="">Seleccione un estado</option>
         <option v-for="estado in uniqueEstados" :key="estado">{{ estado }}</option>
-      </select>
-      <input type="date" v-model="filters.fecha" list="fecha-list">
-      <datalist id="fecha-list">
-        <option v-for="product in products" :value="product.fecha" :key="product._id"/>
-      </datalist>
+      </select> 
       <button @click="exportToPDF" class="button-blue">Exportar a PDF</button>
       <button @click="clearFilters" class="button-blue">Limpiar Filtros</button>
     </div>
-
     
     <!-- Botón para agregar productos -->
     <button @click="openAddModal" class="button-green"><i class="fas fa-plus"></i> Agregar Nuevo Producto</button>
@@ -56,49 +55,34 @@
     </div>
 
 
-    <!-- Modal de Edición -->
-    <div v-if="showEditModal" class="modal">
-      <div class="modal-content">
-        <span class="close" @click="closeEditModal">&times;</span>
-        <h2>Editar Producto</h2>
-        <form @submit.prevent="saveProductChanges" class="form-container">
-          <label for="editNombre">Nombre</label>
-          <input id="editNombre" type="text" v-model="editProductData.nombre">
-          <label for="editCantidad">Categoria</label>
-          <input id="editCantidad" type="text" v-model="editProductData.categoria">
-          <label for="editPrecio">Stock</label>
-          <input id="editPrecio" type="number" v-model="editProductData.stock">
-          <label for="editProveedor">Precio</label>
-          <input id="editProveedor" type="number" v-model="editProductData.precio">
-          <label for="editEstado">Estado</label>
-          <input id="editEstado" type="text" v-model="editProductData.status">
-          <button type="submit" class="button-blue">Guardar Cambios</button>
-        </form>
-      </div>
-    </div>
-
     <!-- Modal de Agregar Nuevo Producto -->
     <div v-if="showAddModal" class="modal">
       <div class="modal-content">
         <span class="close" @click="closeAddModal">&times;</span>
         <h2>Agregar Nuevo Producto</h2>
-        <form @submit.prevent="addNewProduct" class="form-container">
+        <form @submit.prevent="validateAndAddProduct" class="form-container">
           <label for="addNombre">Nombre</label>
           <input id="addNombre" type="text" v-model="newProductData.nombre">
-          <label for="addCantidad">Categoria</label>
-          <input id="addCantidad" type="text" v-model="newProductData.cantidad">
-          <label for="addProveedor">Stock</label>
-          <input id="addProveedor" type="number" v-model="newProductData.stock">
+          <p v-if="newProductErrors.nombre" class="error-message">{{ newProductErrors.nombre }}</p>
+          <label for="addCategoria">Categoría</label>
+          <input id="addCategoria" type="text" v-model="newProductData.categoria">
+          <p v-if="newProductErrors.categoria" class="error-message">{{ newProductErrors.categoria }}</p>
+          <label for="addStock">Stock</label>
+          <input id="addStock" type="number" v-model="newProductData.stock">
+          <p v-if="newProductErrors.stock" class="error-message">{{ newProductErrors.stock }}</p>
           <label for="addPrecio">Precio</label>
           <input id="addPrecio" type="number" v-model="newProductData.precio">
-          <label for="addEstado">Status</label>
-          <input id="addEstado" type="text" v-model="newProductData.status">
+          <p v-if="newProductErrors.precio" class="error-message">{{ newProductErrors.precio }}</p>
+          <label for="addStatus">Estado</label>
+          <input id="addStatus" type="text" v-model="newProductData.status">
+          <p v-if="newProductErrors.status" class="error-message">{{ newProductErrors.status }}</p>
           <button type="submit" class="button-green">Agregar Producto</button>
         </form>
       </div>
     </div>
-        <!-- Notificaciones -->
-        <div v-if="notification.show" :class="['notification', notification.type]">
+    
+    <!-- Notificaciones -->
+    <div v-if="notification.show" :class="['notification', notification.type]">
       <i :class="notificationIcon"></i> {{ notification.message }}
     </div>
   </div>
@@ -111,12 +95,45 @@ import jsPDF from 'jspdf';
 import 'jspdf-autotable';
 
 const products = ref([]);
-const filters = ref({ nombre: '', status: '', categoria: '' });
-const showEditModal = ref(false);
-const editProductData = ref({});
+const filters = ref({ nombre: '', estado: '', categoria: '' });
 const showAddModal = ref(false);
-const newProductData = ref({ nombre: '', categoria: '', stock:0, precio: 0, status: '', });
+const newProductData = ref({ nombre: '', categoria: '', stock: null, precio: null, status: '' });
 const notification = ref({ show: false, message: '', type: '' });
+
+const newProductErrors = ref({ nombre: '', categoria: '', stock: '', precio: '', status: '' });
+
+const validateAndAddProduct = async () => {
+  // Reiniciamos los errores
+  newProductErrors.value = { nombre: '', categoria: '', stock: '', precio: '', status: '' };
+  
+  // Validamos los campos
+  let isValid = true;
+  if (!newProductData.value.nombre.trim()) {
+    newProductErrors.value.nombre = 'El nombre del producto es requerido';
+    isValid = false;
+  }
+  if (!newProductData.value.categoria.trim()) {
+    newProductErrors.value.categoria = 'La categoría del producto es requerida';
+    isValid = false;
+  }
+  if (isNaN(newProductData.value.stock) || newProductData.value.stock === null || newProductData.value.stock < 0) {
+    newProductErrors.value.stock = 'El stock debe ser un número positivo';
+    isValid = false;
+  }
+  if (isNaN(newProductData.value.precio) || newProductData.value.precio === null || newProductData.value.precio < 0) {
+    newProductErrors.value.precio = 'El precio debe ser un número positivo';
+    isValid = false;
+  }
+  if (!newProductData.value.status.trim()) {
+    newProductErrors.value.status = 'El estado del producto es requerido';
+    isValid = false;
+  }
+  
+  // Si no hay errores, agregamos el producto
+  if (isValid) {
+    addNewProduct();
+  }
+};
 
 const notificationIcon = computed(() => {
   return notification.value.type === 'success' ? 'fas fa-check-circle' : 'fas fa-times-circle';
@@ -136,35 +153,35 @@ const fetchProducts = async () => {
 };
 
 const clearFilters = () => {
-  filters.value = { nombre: '', estado: '', fecha: '' };
+  filters.value = { nombre: '', estado: '', categoria: '' };
 };
 
-const uniqueEstados = computed(() => [...new Set(products.value.map(p => p.estado))]);
+const uniqueEstados = computed(() => [...new Set(products.value.map(p => p.status))]);
+const uniqueCategorias = computed(() => [...new Set(products.value.map(p => p.categoria))]);
 
 const filteredProducts = computed(() => {
-  return products.value.filter(p => (!filters.value.producto || p.producto.toLowerCase().includes(filters.value.nombre.toLowerCase())) &&
-                                     (!filters.value.status || p.estado === filters.value.status) &&
-                                     (!filters.value.categoria || new Date(p.categoria).toISOString().slice(0, 10) === filters.value.categoria));
+  let filtered = products.value;
+  if (filters.value.nombre) {
+    filtered = filtered.filter(p => p.nombre.toLowerCase().includes(filters.value.nombre.toLowerCase()));
+  }
+  if (filters.value.estado) {
+    filtered = filtered.filter(p => p.status === filters.value.estado);
+  }
+  if (filters.value.categoria) {
+    filtered = filtered.filter(p => p.categoria === filters.value.categoria);
+  }
+  return filtered;
 });
 
 const statusClass = (status) => {
   switch (status) {
-    case 'Cancelado': return 'status-red';
-    case 'En proceso': return 'status-blue';
-    case 'Entregado': return 'status-green';
-    case 'Pendiente': return 'status-yellow';
-    case 'En tránsito': return 'status-purple';
+    case 'disponible': return 'status-green';
+    case 'poco stock': return 'status-yellow';
+    case 'agotado': return 'status-red';
+    case 'solicitado': return 'status-blue';
+    case 'retirado': return 'status-purple';
     default: return '';
   }
-};
-
-const openEditModal = (product) => {
-  editProductData.value = { ...product };
-  showEditModal.value = true;
-};
-
-const closeEditModal = () => {
-  showEditModal.value = false;
 };
 
 const openAddModal = () => {
@@ -173,6 +190,17 @@ const openAddModal = () => {
 
 const closeAddModal = () => {
   showAddModal.value = false;
+};
+
+const deleteProduct = async (id) => {
+  try {
+    await eliminarProducto(id);
+    products.value = products.value.filter((product) => product._id !== id);
+    notify('success', 'Producto eliminado exitosamente');
+  } catch (error) {
+    console.error('Error al eliminar producto:', error.message);
+    notify('error', 'Error al eliminar producto');
+  }
 };
 
 const addNewProduct = async () => {
@@ -187,36 +215,127 @@ const addNewProduct = async () => {
   }
 };
 
-const saveProductChanges = async () => {
-  try {
-    const updatedProduct = await actualizarProducto(editProductData.value._id, editProductData.value);
-    const index = products.value.findIndex(p => p._id === editProductData.value._id);
-    if (index !== -1) {
-      products.value[index] = updatedProduct;
-    }
-    showEditModal.value = false;
-    notify('success', 'Producto actualizado exitosamente');
-  } catch (error) {
-    console.error('Error al guardar cambios:', error.message);
-    notify('error', 'Error al guardar cambios');
-  }
-};
-
 const exportToPDF = () => {
-  const doc = new jsPDF();
-  doc.text('Productos', 10, 10);
-  doc.autoTable({
-    head: [['ID', 'Nombre', 'Cantidad', 'Precio', 'Proveedor', 'Método de pago', 'Estado', 'Fecha']],
-    body: filteredProducts.value.map(p => [p._id, p.producto, p.cantidad.toString(), `$${p.precio.toFixed(2)}`, p.proveedor, p.metodo_pago, p.estado, new Date(p.fecha).toLocaleDateString()]),
-    startY: 20,
-  });
-  doc.save('productos.pdf');
+  try {
+    const doc = new jsPDF();
+    doc.text('Lista de Productos', 10, 10);
+    let tableData = [];
+
+    if (filteredProducts.value.length > 0) {
+      tableData = filteredProducts.value.map(p => {
+        return [
+          p._id,
+          p.nombre,
+          p.categoria,
+          p.stock,
+          p.precio != null ? `$${p.precio.toFixed(2)}` : '', 
+          p.status
+        ];
+      });
+    } else {
+      tableData = products.value.map(p => {
+        return [
+          p._id,
+          p.nombre,
+          p.categoria,
+          p.stock,
+          p.precio != null ? `$${p.precio.toFixed(2)}` : '', // Verificación de null
+          p.status
+        ];
+      });
+    }
+
+    doc.autoTable({
+      head: [['ID', 'Nombre', 'Categoría', 'Stock', 'Precio', 'Estado']],
+      body: tableData,
+      startY: 20,
+    });
+    doc.save('productos.pdf');
+  } catch (error) {
+    console.error('Error al exportar a PDF:', error.message);
+    notify('error', 'Error al exportar a PDF');
+  }
 };
 
 onMounted(fetchProducts);
 </script>
 
+
 <style>
+.error-message {
+  color: red;
+  margin-top: 5px;
+  font-size: 12px;
+}
+/* Estilos para el modal de edición y el modal de agregar nuevo producto */
+.modal {
+  display: none; /* Ocultamos el modal por defecto */
+  position: fixed;
+  z-index: 1000;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  background-color: rgba(0, 0, 0, 0.5);
+}
+
+.modal-content {
+  background-color: #fefefe;
+  margin: 10% auto;
+  padding: 20px;
+  border: 1px solid #888;
+  width: 50%;
+  border-radius: 8px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); /* Agregamos sombra */
+}
+
+.close {
+  color: #aaa;
+  float: right;
+  font-size: 28px;
+  font-weight: bold;
+  cursor: pointer;
+}
+
+.close:hover,
+.close:focus {
+  color: black;
+  text-decoration: none;
+}
+
+/* Estilos para el formulario dentro del modal */
+.form-container {
+  display: grid;
+  gap: 15px;
+}
+
+.form-container label {
+  font-weight: bold;
+}
+
+.form-container input[type="text"],
+.form-container input[type="number"] {
+  width: calc(100% - 20px);
+  padding: 8px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+}
+
+.form-container button[type="submit"] {
+  width: 100%;
+  padding: 10px;
+  border: none;
+  border-radius: 4px;
+  background-color: #007bff;
+  color: white;
+  cursor: pointer;
+  transition: background-color 0.3s;
+}
+
+.form-container button[type="submit"]:hover {
+  background-color: #0056b3;
+}
 .icon-button {
   background-color: transparent;
   border: none;
@@ -230,8 +349,8 @@ onMounted(fetchProducts);
 }
 
 .product-container {
-  width: 80%;
-  margin-right: 100px;
+  margin-left: -320px;
+  width: 90%;
   padding: 20px;
 }
 
@@ -329,11 +448,12 @@ th:hover {
   text-decoration: none;
 }
 
-.status-red { color: #ff0000; } /* Cancelado */
-.status-blue { color: #007bff; } /* En proceso */
-.status-green { color: #28a745; } /* Entregado */
-.status-yellow { color: #ffff00; } /* Pendiente */
-.status-purple { color: #800080; } /* En tránsito */
+.status-red { color: #ff0000; } /* Disponible */
+.status-blue { color: #007bff; } /* Poco stock */
+.status-green { color: #28a745; } /* Agotado */
+.status-yellow { color: #ffff00; } /* Solicitado */
+.status-purple { color: #800080; } /* Retirado */
+
 
 .notification {
   position: fixed;
